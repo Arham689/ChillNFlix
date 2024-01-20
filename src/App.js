395674e3b39b45337bd081573dfff107947ca1d5
Arham689 <98389773+Arham689@users.noreply.github.com>
2024-01-20 22:44:22 +0530

    First commit and tailwind added🔹

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="font-bold text-[100px]">
+      <h1 className='bg-zinc-600 text-red-500 '>hello lelo</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+/*
+  only affects in development mode not in prodtion !
+  #StrictMode can affect in 6 different types  
+    -unintended side effect (React intentionally invokes certain functions twice to help catch unintentional side-effects.)
+    -detect mount and unmount (use cleanup code in useEffect )
+    -can detect depriciated code     
+    - Identifying Unsafe Lifecycles:
+ 
+ */
+
