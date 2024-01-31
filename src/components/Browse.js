106043721662f4d1cd0c9ee74d491d3a6ceefa2f
Arham689 <98389773+Arham689@users.noreply.github.com>
2024-01-31 22:13:20 +0530

    Footer added

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -6,6 +6,7 @@ import useGetPopularMovies from '../Hooks/useGetPopularMovies'
 import useGetUpComingMovies from '../Hooks/useGetUpComingMovies'
 import MainVideoBackground from './MainVideoBackground'
 import SecondaryVideoList from './SecondaryVideoList'
+import Footer from './Footer'
 
 const Browse = () => {
   
@@ -18,6 +19,7 @@ const Browse = () => {
       <Header/>
       <MainVideoBackground/>
       <SecondaryVideoList/>
+      <Footer/>
     </div>
   )
 }
